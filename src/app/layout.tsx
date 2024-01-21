8c8176557c203699f03a39c29ff6880044feb6e6
Arpit Gupta <gupta98960@gmail.com>
2024-01-21 19:58:41 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body
+        className={cn("h-full relative antialiased font-sans", inter.className)}
+      >
+        
+        {/* The classes in the main tag ensures that our main components take the full scrren so that when there is no content footer remains at the bottom */}
+        <main className="relative flex flex-col min-h-screen">
+          <div className="flex-grow flex-1">{children}</div>
+        </main>
+      </body>
     </html>
   );
 }
